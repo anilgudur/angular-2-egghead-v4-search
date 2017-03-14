2ec9ae8b3c041b911849f75c08a84fab63531161
Anil <anil.gudur@clariontechnologies.co.in>
2017-03-14 19:30:47 +0530

    Initial set up with v3 Log Bedugger

---
+++ src/app/app.module.ts
@@ -0,0 +1,23 @@
+import { NgModule }      from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent }  from './app.component';
+import { CONFIG_PROVIDERS, IS_ENABLED_LOG_DEBUGGER } from './core/config/config.providers';
+import { ListModule }   from './list/list.module';
+import { ListServiceModule }   from './list/list-service.module';
+import { LogsModule } from './core/logs/logs.module';
+
+@NgModule({
+  imports: [ 
+      BrowserModule, 
+      ListModule, 
+      ListServiceModule.forRoot(),
+      LogsModule.forRoot()
+  ],
+  declarations: [ AppComponent ],
+  bootstrap:    [ AppComponent ],
+  providers: [
+    CONFIG_PROVIDERS
+  ]
+})
+export class AppModule { }
